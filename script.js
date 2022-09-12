0a195ec84c931b7d0296e4d590d099a0f318fd9b
EmD4blju <92537261+EmD4blju@users.noreply.github.com>
2022-09-12 14:16:19 +0200

    12.09.2022
    - clock replacement,
    - added about me section in
       about_me.html

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 window.onload = function(){
-     setInterval(function(){
-        var time = new Date();
-        var hours = time.getHours();
-        var minutes = time.getMinutes();
-        var seconds = time.getSeconds();
+    setInterval(function(){
+       var time = new Date();
+       var hours = time.getHours();
+       var minutes = time.getMinutes();
+       var seconds = time.getSeconds();
 
-        if(hours < 10){
-            document.getElementById("clock").innerHTML = "0" + hours + " : " + minutes + " : " + seconds;
-        }else if(minutes < 10){
-            document.getElementById("clock").innerHTML = hours + " : 0" + minutes + " : " + seconds;
-        }else if (seconds < 10){
-            document.getElementById("clock").innerHTML = hours + " : " + minutes + " : 0" + seconds;
-        }else document.getElementById("clock").innerHTML = hours + " : " + minutes + " : " + seconds;
-        
-    }, 1000);
+       if(hours < 10){
+           document.getElementById("clock").innerHTML = "0" + hours + " : " + minutes + " : " + seconds;
+       }else if(minutes < 10){
+           document.getElementById("clock").innerHTML = hours + " : 0" + minutes + " : " + seconds;
+       }else if (seconds < 10){
+           document.getElementById("clock").innerHTML = hours + " : " + minutes + " : 0" + seconds;
+       }else document.getElementById("clock").innerHTML = hours + " : " + minutes + " : " + seconds;
+       
+   }, 1000);
 }
 
 let toggle = document.querySelector('#toggle');
